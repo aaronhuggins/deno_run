cfdc9d3ee86a3a58e881b41ca44ffea071205f8b
Aaron Huggins <aaron.huggins@gmail.com>
2020-09-11 08:58:30 -0500

    Remove deprecated method

--- lib/terminal.ts
+++ lib/terminal.ts
@@ -1,32 +1,3 @@
-function objectToSource (val: any) {
-  const CIRC_REF = '[Circular]'
-
-  function circular () {
-    const visited = new Set()
-
-    return function replacer (key: string, value: any) {
-      if (typeof value === 'object' && value !== null) {
-        if (visited.has(value)) {
-          return CIRC_REF
-        }
-
-        visited.add(value)
-      }
-
-      if (typeof value === 'function') {
-        return `[Function: ${typeof value.name === 'undefined' ? key : value.name}]`
-      }
-
-      return value
-    }
-  }
-
-  return JSON.stringify(val, circular(), 2)
-    .replace(/"([a-zA-Z_][a-zA-Z0-9_]*)":/gu, '$1:')
-    .replace(/"\[Function: ([a-zA-Z_][a-zA-Z0-9_]*)\]"/gu, '[Function: $1]')
-    .replace(/"\[Circular\]"/gu, CIRC_REF)
-}
-
 export const { log: message } = console
 
 export function prompt(message: string = '') {
