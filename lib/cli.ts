0e9c6b5c91a1edac583927416136411aee0cef2d
Aaron Huggins <aaron.huggins@gmail.com>
2020-09-10 22:30:25 -0500

    fix detection of base url for manifest

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,5 +1,5 @@
 import { parser, Arguments } from '../deps.ts'
-import { MANIFEST_FNAME } from './helpers.ts'
+import { MANIFEST_FNAME, isUrl } from './helpers.ts'
 import manifest from '../manifest.ts'
 
 export type CliCommand = 'run' | 'install' | 'display' | 'help' | 'upgrade' | 'bootstrap'
@@ -72,7 +72,7 @@ export function getArgs (): { deno: string[], dr: string[], script: string[] } {
       continue
     }
 
-    if (!isScript && args[i].endsWith(MANIFEST_FNAME)) {
+    if (!isScript && (args[i].endsWith(MANIFEST_FNAME) || isUrl(args[i]))) {
       dr.push(FLAG.MFST, args[i])
       isScript = true
 
